95f5761a3f2eed5dadd7570ff4a4d2db92c3a2f4
Youness-Ailal <itsyounessaylal@gmail.com>
2023-12-13 22:48:11 +0100

    initial commit

--- src/components/BookmarkButton.jsx
+++ src/components/BookmarkButton.jsx
@@ -2,7 +2,7 @@
 import { FaRegBookmark } from "react-icons/fa6"; // to bookmark
 import { FaBookmark } from "react-icons/fa6"; //bookmarked
 
-import "../styles/bookmarkButton.scss";
+import "../styles/BookmarkButton.scss";
 function BookmarkButton({ bookmark, onClick }) {
   return (
     <button onClick={onClick} className="bookmark-btn">
